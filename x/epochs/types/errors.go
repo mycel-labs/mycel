003c366fe55da047d2da42a17e13043fbdc7956e
taryune <taryunelab@gmail.com>
2023-03-23 19:16:08 +0100

    add epoch types

--- x/epochs/types/errors.go
+++ x/epochs/types/errors.go
@@ -8,5 +8,10 @@ import (
 
 // x/epochs module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrSample                                  = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrEpochIdentifierCannotBeEmpty            = sdkerrors.Register(ModuleName, 1101, "epoch identifier cannot be empty")
+	ErrEpochDurationCannotBeZero               = sdkerrors.Register(ModuleName, 1102, "epoch duration cannot be zero")
+	ErrCurrentEpochCannotBeNegative            = sdkerrors.Register(ModuleName, 1103, "current epoch cannot be negative")
+	ErrCurrentEpochStartHeightCannotBeNegative = sdkerrors.Register(ModuleName, 1104, "current epoch start height cannot be negative")
+	ErrDuplicatedEpochEntry                    = sdkerrors.Register(ModuleName, 1105, "duplicated epoch entry")
 )
