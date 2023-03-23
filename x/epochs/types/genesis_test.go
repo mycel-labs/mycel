e7bbff80614cc9e3843b13d3c99b11bfc32c0ef4
taryune <taryunelab@gmail.com>
2023-03-23 18:18:53 +0100

    add epoch proto

--- x/epochs/types/genesis_test.go
+++ x/epochs/types/genesis_test.go
@@ -19,12 +19,34 @@ func TestGenesisState_Validate(t *testing.T) {
 			valid:    true,
 		},
 		{
-			desc:     "valid genesis state",
+			desc: "valid genesis state",
 			genState: &types.GenesisState{
 
+				EpochInfoList: []types.EpochInfo{
+					{
+						Identifier: "0",
+					},
+					{
+						Identifier: "1",
+					},
+				},
 				// this line is used by starport scaffolding # types/genesis/validField
 			},
 			valid: true,
+		},
+		{
+			desc: "duplicated epochInfo",
+			genState: &types.GenesisState{
+				EpochInfoList: []types.EpochInfo{
+					{
+						Identifier: "0",
+					},
+					{
+						Identifier: "0",
+					},
+				},
+			},
+			valid: false,
 		},
 		// this line is used by starport scaffolding # types/genesis/testcase
 	} {
