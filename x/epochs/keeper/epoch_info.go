ddde6213be269c3ee3b2289c560653622b4f2a91
taryune <taryunelab@gmail.com>
2023-03-23 18:18:53 +0100

    add epoch proto

---
+++ x/epochs/keeper/epoch_info.go
@@ -0,0 +1,63 @@
+package keeper
+
+import (
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"mycel/x/epochs/types"
+)
+
+// SetEpochInfo set a specific epochInfo in the store from its index
+func (k Keeper) SetEpochInfo(ctx sdk.Context, epochInfo types.EpochInfo) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.EpochInfoKeyPrefix))
+	b := k.cdc.MustMarshal(&epochInfo)
+	store.Set(types.EpochInfoKey(
+		epochInfo.Identifier,
+	), b)
+}
+
+// GetEpochInfo returns a epochInfo from its index
+func (k Keeper) GetEpochInfo(
+	ctx sdk.Context,
+	identifier string,
+
+) (val types.EpochInfo, found bool) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.EpochInfoKeyPrefix))
+
+	b := store.Get(types.EpochInfoKey(
+		identifier,
+	))
+	if b == nil {
+		return val, false
+	}
+
+	k.cdc.MustUnmarshal(b, &val)
+	return val, true
+}
+
+// RemoveEpochInfo removes a epochInfo from the store
+func (k Keeper) RemoveEpochInfo(
+	ctx sdk.Context,
+	identifier string,
+
+) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.EpochInfoKeyPrefix))
+	store.Delete(types.EpochInfoKey(
+		identifier,
+	))
+}
+
+// GetAllEpochInfo returns all epochInfo
+func (k Keeper) GetAllEpochInfo(ctx sdk.Context) (list []types.EpochInfo) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.EpochInfoKeyPrefix))
+	iterator := sdk.KVStorePrefixIterator(store, []byte{})
+
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		var val types.EpochInfo
+		k.cdc.MustUnmarshal(iterator.Value(), &val)
+		list = append(list, val)
+	}
+
+	return
+}
