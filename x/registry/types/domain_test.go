8653ee5b350aac4f822a75299b8a8128b84f070f
taryune <taryunelab@gmail.com>
2023-03-31 16:38:11 +0200

    refactor domain validation test

--- x/registry/types/domain_test.go
+++ x/registry/types/domain_test.go
@@ -22,6 +22,118 @@ type WalletRecordTest struct {
 	IsInvalidValue      bool
 }
 
+func TestDomainValidate(t *testing.T) {
+	testCases := []struct {
+		domain          Domain
+		expDomainLevel  int
+		expDomainParent Domain
+		expDomainPrice  sdk.Coin
+		expErr          string
+	}{
+		// Valid domains
+		{
+			domain:          Domain{Name: "foo", Parent: "myc"},
+			expDomainLevel:  2,
+			expDomainParent: Domain{Name: "myc", Parent: ""},
+			expDomainPrice:  sdk.NewCoin("MYCEL", sdk.NewInt(30_300)),
+			expErr:          "",
+		},
+		{
+			domain:          Domain{Name: "12345", Parent: ""},
+			expDomainLevel:  1,
+			expDomainParent: Domain{Name: "", Parent: ""},
+			expDomainPrice:  sdk.NewCoin("MYCEL", sdk.NewInt(303)),
+			expErr:          "",
+		},
+		{
+			domain:          Domain{Name: "1234", Parent: "foo.myc"},
+			expDomainLevel:  3,
+			expDomainParent: Domain{Name: "foo", Parent: "myc"},
+			expDomainPrice:  sdk.NewCoin("MYCEL", sdk.NewInt(3_030)),
+			expErr:          "",
+		},
+		{
+			domain:          Domain{Name: "123", Parent: "foo.myc"},
+			expDomainLevel:  3,
+			expDomainParent: Domain{Name: "foo", Parent: "myc"},
+			expDomainPrice:  sdk.NewCoin("MYCEL", sdk.NewInt(30_300)),
+			expErr:          "",
+		},
+		{
+			domain:          Domain{Name: "12", Parent: "foo.myc"},
+			expDomainLevel:  3,
+			expDomainParent: Domain{Name: "foo", Parent: "myc"},
+			expDomainPrice:  sdk.NewCoin("MYCEL", sdk.NewInt(303_000)),
+			expErr:          "",
+		},
+		{
+			domain:          Domain{Name: "🍭", Parent: "foo.🍭"},
+			expDomainLevel:  3,
+			expDomainParent: Domain{Name: "foo", Parent: "🍭"},
+			expDomainPrice:  sdk.NewCoin("MYCEL", sdk.NewInt(3_030_000)),
+			expErr:          "",
+		},
+		{
+			domain:          Domain{Name: "🍭", Parent: "foo.🍭.myc"},
+			expDomainLevel:  4,
+			expDomainParent: Domain{Name: "foo.🍭", Parent: "myc"},
+			expDomainPrice:  sdk.NewCoin("MYCEL", sdk.NewInt(3_030_000)),
+			expErr:          "",
+		},
+		// Invalid name
+		{domain: Domain{Name: ".foo", Parent: "myc"},
+			expErr: fmt.Sprintf("invalid name: .foo"),
+		},
+		{domain: Domain{Name: "", Parent: "myc"},
+			expErr: fmt.Sprintf("invalid name: "),
+		},
+		{domain: Domain{Name: "bar.foo", Parent: "myc"},
+			expErr: fmt.Sprintf("invalid name: bar.foo"),
+		},
+		{domain: Domain{Name: ".", Parent: "myc"},
+			expErr: fmt.Sprintf("invalid name: ."),
+		},
+		{domain: Domain{Name: "##", Parent: "myc"},
+			expErr: fmt.Sprintf("invalid name: ##"),
+		},
+		// Invalid parent
+		{
+			domain: Domain{Name: "foo", Parent: ".##"},
+			expErr: fmt.Sprintf("invalid parent: .##"),
+		},
+		{
+			domain: Domain{Name: "foo", Parent: ".myc"},
+			expErr: fmt.Sprintf("invalid parent: .myc"),
+		},
+		{
+			domain: Domain{Name: "foo", Parent: ".foo.myc"},
+			expErr: fmt.Sprintf("invalid parent: .foo.myc"),
+		},
+	}
+
+	for _, tc := range testCases {
+		err := tc.domain.ValidateDomain()
+		if tc.expErr == "" {
+			require.Nil(t, err)
+			// Check domain level
+			require.Equal(t, tc.expDomainLevel, tc.domain.GetDomainLevel())
+
+			// Check domain parent
+			name, parent := tc.domain.ParseParent()
+			require.Equal(t, tc.expDomainParent.Name, name)
+			require.Equal(t, tc.expDomainParent.Parent, parent)
+
+			// Check domain price
+			require.Equal(t, tc.expDomainPrice, tc.domain.GetRegistrationFee())
+
+		} else {
+			require.EqualError(t, err, tc.expErr)
+		}
+
+	}
+
+}
+
 func GetValidDomains() []DomainTest {
 	return []DomainTest{
 		{Domain: Domain{Name: "foo", Parent: "myc"},
@@ -91,66 +203,6 @@ func GetInvalidUpdateWalletRecords() []WalletRecordTest {
 	}
 }
 
-func TestValidateDomainNameSuccess(t *testing.T) {
-	for _, v := range GetValidDomains() {
-		err := v.Domain.ValidateDomain()
-		require.Nil(t, err)
-	}
-}
-func TestValidateDomainNameFailure(t *testing.T) {
-	for _, v := range GetInvalidNameDomains() {
-		err := v.Domain.ValidateDomainName()
-		require.EqualError(t, err, fmt.Sprintf("invalid name: %s", v.Domain.Name))
-	}
-}
-
-func TestValidateDomainParentSuccess(t *testing.T) {
-	for _, v := range GetValidDomains() {
-		err := v.Domain.ValidateDomainParent()
-		require.Nil(t, err)
-	}
-}
-
-func TestValidateDomainParentFailure(t *testing.T) {
-	for _, v := range GetInvalidParentDomains() {
-		err := v.Domain.ValidateDomainParent()
-		require.EqualError(t, err, fmt.Sprintf("invalid parent: %s", v.Domain.Parent))
-	}
-}
-
-func TestValidateDomainSuccess(t *testing.T) {
-	for _, v := range GetValidDomains() {
-		err := v.Domain.ValidateDomain()
-		require.Nil(t, err)
-	}
-}
-
-func TestValidateDomainFailure(t *testing.T) {
-	for _, v := range GetInvalidNameDomains() {
-		err := v.Domain.ValidateDomain()
-		require.EqualError(t, err, fmt.Sprintf("invalid name: %s", v.Domain.Name))
-	}
-	for _, v := range GetInvalidParentDomains() {
-		err := v.Domain.ValidateDomainParent()
-		require.EqualError(t, err, fmt.Sprintf("invalid parent: %s", v.Domain.Parent))
-	}
-}
-
-func TestGetDomainLevel(t *testing.T) {
-	for _, v := range GetValidDomains() {
-		domainLevel := v.Domain.GetDomainLevel()
-		require.Equal(t, domainLevel, v.DomainLevel)
-	}
-}
-
-func TestGetDomainParent(t *testing.T) {
-	for _, v := range GetValidDomains() {
-		name, parent := v.Domain.ParseParent()
-		require.Equal(t, v.DomainParent.Name, name)
-		require.Equal(t, v.DomainParent.Parent, parent)
-	}
-}
-
 func TestValidateWalletAddressSuccess(t *testing.T) {
 	for _, walletRecord := range GetValidUpdateWalletRecords() {
 		addressFormat, errAddressFormat := GetWalletAddressFormat(walletRecord.WalletRecordType)
@@ -207,12 +259,5 @@ func TestUpdateWalletRecordFailure(t *testing.T) {
 			addressFormat, _ := GetWalletAddressFormat(walletRecord.WalletRecordType)
 			require.EqualError(t, err, fmt.Sprintf("invalid wallet address: %s %s", addressFormat, walletRecord.Address))
 		}
-	}
-}
-
-func TestGetRegistrationFee(t *testing.T) {
-	for _, domain := range GetValidDomains() {
-		fee := domain.Domain.GetRegistrationFee()
-		require.Equal(t, domain.DomainPrice, fee)
 	}
 }
