33d4742e407a4544629014e0264265b8d4e6d1bc
taryune <taryunelab@gmail.com>
2023-04-11 18:37:07 +0900

    generate react/ts

---
+++ ts-client/mycel.registry/types.ts
@@ -0,0 +1,13 @@
+import { DNSRecord } from "./types/mycel/registry/domain"
+import { WalletRecord } from "./types/mycel/registry/domain"
+import { Domain } from "./types/mycel/registry/domain"
+import { Params } from "./types/mycel/registry/params"
+
+
+export {     
+    DNSRecord,
+    WalletRecord,
+    Domain,
+    Params,
+    
+ }
