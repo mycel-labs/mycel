fdd5cde5042eab867cc619e974653d0e0ccb3c7e
silmin <silvestmint@gmail.com>
2023-04-13 07:47:28 +0900

    impl update-dns-record subcommand to myceld CLI

---
+++ x/registry/client/cli/tx_update_dns_record.go
@@ -0,0 +1,48 @@
+package cli
+
+import (
+	"mycel/x/registry/types"
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cobra"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdUpdateDnsRecord() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update-dns-record [name] [parent] [dns-record-type] [value]",
+		Short: "Broadcast message updateDnsRecord",
+		Args:  cobra.ExactArgs(4),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			argName := args[0]
+			argParent := args[1]
+			argDnsRecordType := args[2]
+			argValue := args[3]
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgUpdateDnsRecord(
+				clientCtx.GetFromAddress().String(),
+				argName,
+				argParent,
+				argDnsRecordType,
+				argValue,
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
