046304de16cf4a738c043966ed82a71a2fa36eff
totegamma <github@gammalab.net>
2023-04-15 19:53:14 +0900

    add explore view

---
+++ react/src/views/ExploreView.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import IgntAssets from "../components/IgntAssets";
+import IgntTransactions from "../components/IgntTransactions";
+import IgntTransfer from "../components/IgntTransfer";
+
+export default function ExploreView() {
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2">
+          Explore dayo
+        </div>
+      </div>
+    </div>
+  );
+}
