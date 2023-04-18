572daf79e8074ba953b4d95d2a077b98db1241a3
taryune <taryunelab@gmail.com>
2023-04-18 17:26:05 +0900

    change token denom MYCEL->mycel

--- x/registry/types/message_update_dns_record_test.go
+++ x/registry/types/message_update_dns_record_test.go
@@ -3,7 +3,7 @@ package types
 import (
 	"testing"
 
-	"mycel/testutil/sample"
+	"github.com/mycel-domain/mycel/testutil/sample"
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
