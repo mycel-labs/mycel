5ef3f585236a3b4ce15f5f9b2fb1d99897020ec7
silmin <silvestmint@gmail.com>
2023-04-18 16:49:25 +0900

    fix param for demo data

--- demo_data.sh
+++ demo_data.sh
@@ -17,6 +17,6 @@ myceld tx registry update-dns-record bar cel A 10.0.0.1 --from=alice -y
 # update wallet records
 myceld tx registry update-wallet-record foo cel ETHEREUM_MAINNET 0x1234567890123456789012345678901234567890 --from=alice -y
 myceld tx registry update-wallet-record foo cel POLYGON_MAINNET 0x1234567890123456789012345678901234567890 --from=alice -y
-myceld tx registry update-wallet-record foo.cel ETHEREUM_GOERLI 0x61b08DcE6751E5329984BD9098464Ee00c30984b --from=alice -y
+myceld tx registry update-wallet-record foo cel ETHEREUM_GOERLI 0x61b08DcE6751E5329984BD9098464Ee00c30984b --from=alice -y
 myceld tx registry update-wallet-record hoge foo.cel ETHEREUM_MAINNET 0x1234567890123456789012345678901234567890 --from=alice -y
 myceld tx registry update-wallet-record bar cel ETHEREUM_MAINNET 0x1234567890123456789012345678901234567890 --from=alice -y
