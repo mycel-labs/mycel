650bf456ec5abd7b8f654cc077f60babda1d27de
taryune <taryunelab@gmail.com>
2023-04-27 19:26:41 +0900

    rm ts and react

--- ts-client/mycel.epochs/types.ts
+++
@@ -1,9 +0,0 @@
-import { EpochInfo } from "./types/mycel/epochs/epoch_info"
-import { Params } from "./types/mycel/epochs/params"
-
-
-export {     
-    EpochInfo,
-    Params,
-    
- }
--- ts-client/node_modules/@keplr-wallet/types/build/secretjs.d.ts
+++
@@ -1,6 +0,0 @@
-export interface SecretUtils {
-    getPubkey: () => Promise<Uint8Array>;
-    decrypt: (ciphertext: Uint8Array, nonce: Uint8Array) => Promise<Uint8Array>;
-    encrypt: (contractCodeHash: string, msg: object) => Promise<Uint8Array>;
-    getTxEncryptionKey: (nonce: Uint8Array) => Promise<Uint8Array>;
-}
