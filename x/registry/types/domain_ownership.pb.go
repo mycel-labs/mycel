095b04db4c8f6c265100433e9a3400959db9381d
taryune <taryunelab@gmail.com>
2023-05-30 13:26:17 +0200

    change key type to address

--- x/registry/types/domain_ownership.pb.go
+++ x/registry/types/domain_ownership.pb.go
@@ -5,6 +5,7 @@ package types
 
 import (
 	fmt "fmt"
+	_ "github.com/cosmos/cosmos-proto"
 	proto "github.com/gogo/protobuf/proto"
 	io "io"
 	math "math"
@@ -22,16 +23,68 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
+type OwnedDomain struct {
+	Name   string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	Parent string `protobuf:"bytes,2,opt,name=parent,proto3" json:"parent,omitempty"`
+}
+
+func (m *OwnedDomain) Reset()         { *m = OwnedDomain{} }
+func (m *OwnedDomain) String() string { return proto.CompactTextString(m) }
+func (*OwnedDomain) ProtoMessage()    {}
+func (*OwnedDomain) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a18547d3c76fcc7b, []int{0}
+}
+func (m *OwnedDomain) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *OwnedDomain) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_OwnedDomain.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *OwnedDomain) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_OwnedDomain.Merge(m, src)
+}
+func (m *OwnedDomain) XXX_Size() int {
+	return m.Size()
+}
+func (m *OwnedDomain) XXX_DiscardUnknown() {
+	xxx_messageInfo_OwnedDomain.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_OwnedDomain proto.InternalMessageInfo
+
+func (m *OwnedDomain) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+func (m *OwnedDomain) GetParent() string {
+	if m != nil {
+		return m.Parent
+	}
+	return ""
+}
+
 type DomainOwnership struct {
-	Owner   string   `protobuf:"bytes,1,opt,name=owner,proto3" json:"owner,omitempty"`
-	Domains []string `protobuf:"bytes,2,rep,name=domains,proto3" json:"domains,omitempty"`
+	Owner   string         `protobuf:"bytes,1,opt,name=owner,proto3" json:"owner,omitempty"`
+	Domains []*OwnedDomain `protobuf:"bytes,2,rep,name=domains,proto3" json:"domains,omitempty"`
 }
 
 func (m *DomainOwnership) Reset()         { *m = DomainOwnership{} }
 func (m *DomainOwnership) String() string { return proto.CompactTextString(m) }
 func (*DomainOwnership) ProtoMessage()    {}
 func (*DomainOwnership) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a18547d3c76fcc7b, []int{0}
+	return fileDescriptor_a18547d3c76fcc7b, []int{1}
 }
 func (m *DomainOwnership) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -67,7 +120,7 @@ func (m *DomainOwnership) GetOwner() string {
 	return ""
 }
 
-func (m *DomainOwnership) GetDomains() []string {
+func (m *DomainOwnership) GetDomains() []*OwnedDomain {
 	if m != nil {
 		return m.Domains
 	}
@@ -75,7 +128,8 @@ func (m *DomainOwnership) GetDomains() []string {
 }
 
 func init() {
-	proto.RegisterType((*DomainOwnership)(nil), "myceldomain.mycel.registry.DomainOwnership")
+	proto.RegisterType((*OwnedDomain)(nil), "mycel.registry.OwnedDomain")
+	proto.RegisterType((*DomainOwnership)(nil), "mycel.registry.DomainOwnership")
 }
 
 func init() {
@@ -83,19 +137,61 @@ func init() {
 }
 
 var fileDescriptor_a18547d3c76fcc7b = []byte{
-	// 177 bytes of a gzipped FileDescriptorProto
+	// 259 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0xcd, 0xad, 0x4c, 0x4e,
 	0xcd, 0xd1, 0x2f, 0x4a, 0x4d, 0xcf, 0x2c, 0x2e, 0x29, 0xaa, 0xd4, 0x4f, 0xc9, 0xcf, 0x4d, 0xcc,
 	0xcc, 0x8b, 0xcf, 0x2f, 0xcf, 0x4b, 0x2d, 0x2a, 0xce, 0xc8, 0x2c, 0xd0, 0x2b, 0x28, 0xca, 0x2f,
-	0xc9, 0x17, 0x92, 0x02, 0x2b, 0x83, 0x48, 0xea, 0x81, 0xd9, 0x7a, 0x30, 0x2d, 0x4a, 0x8e, 0x5c,
-	0xfc, 0x2e, 0x60, 0x09, 0x7f, 0x98, 0x26, 0x21, 0x11, 0x2e, 0x56, 0xb0, 0x09, 0x12, 0x8c, 0x0a,
-	0x8c, 0x1a, 0x9c, 0x41, 0x10, 0x8e, 0x90, 0x04, 0x17, 0x3b, 0xc4, 0x84, 0x62, 0x09, 0x26, 0x05,
-	0x66, 0x0d, 0xce, 0x20, 0x18, 0xd7, 0xc9, 0xe3, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18,
-	0x1f, 0x3c, 0x92, 0x63, 0x9c, 0xf0, 0x58, 0x8e, 0xe1, 0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5,
-	0x18, 0xa2, 0xf4, 0xd2, 0x33, 0x4b, 0x32, 0x4a, 0x93, 0xf4, 0x92, 0xf3, 0x73, 0xf5, 0xc1, 0xf6,
-	0xea, 0x42, 0xf4, 0x40, 0x38, 0xfa, 0x15, 0x08, 0x97, 0x97, 0x54, 0x16, 0xa4, 0x16, 0x27, 0xb1,
-	0x81, 0xdd, 0x6b, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0xb3, 0x48, 0xea, 0x9c, 0xd8, 0x00, 0x00,
-	0x00,
+	0xc9, 0x17, 0xe2, 0x03, 0x2b, 0xd3, 0x83, 0x29, 0x93, 0x92, 0x4c, 0xce, 0x2f, 0xce, 0xcd, 0x2f,
+	0x8e, 0x07, 0xcb, 0xea, 0x43, 0x38, 0x10, 0xa5, 0x4a, 0x96, 0x5c, 0xdc, 0xfe, 0xe5, 0x79, 0xa9,
+	0x29, 0x2e, 0x60, 0x93, 0x84, 0x84, 0xb8, 0x58, 0xf2, 0x12, 0x73, 0x53, 0x25, 0x18, 0x15, 0x18,
+	0x35, 0x38, 0x83, 0xc0, 0x6c, 0x21, 0x31, 0x2e, 0xb6, 0x82, 0xc4, 0xa2, 0xd4, 0xbc, 0x12, 0x09,
+	0x26, 0xb0, 0x28, 0x94, 0xa7, 0x54, 0xc1, 0xc5, 0x0f, 0xd1, 0xe5, 0x0f, 0xb3, 0x5e, 0x48, 0x8f,
+	0x8b, 0x15, 0xec, 0x16, 0x88, 0x7e, 0x27, 0x89, 0x4b, 0x5b, 0x74, 0x45, 0xa0, 0xd6, 0x39, 0xa6,
+	0xa4, 0x14, 0xa5, 0x16, 0x17, 0x07, 0x97, 0x14, 0x65, 0xe6, 0xa5, 0x07, 0x41, 0x94, 0x09, 0x99,
+	0x72, 0xb1, 0x43, 0xbc, 0x50, 0x2c, 0xc1, 0xa4, 0xc0, 0xac, 0xc1, 0x6d, 0x24, 0xad, 0x87, 0xea,
+	0x74, 0x3d, 0x24, 0xc7, 0x05, 0xc1, 0xd4, 0x3a, 0x79, 0x9c, 0x78, 0x24, 0xc7, 0x78, 0xe1, 0x91,
+	0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78, 0x2c, 0xc7, 0x70, 0xe3,
+	0xb1, 0x1c, 0x43, 0x94, 0x5e, 0x7a, 0x66, 0x49, 0x46, 0x69, 0x92, 0x5e, 0x72, 0x7e, 0xae, 0x3e,
+	0xd8, 0x24, 0x5d, 0x88, 0x1e, 0x08, 0x47, 0xbf, 0x02, 0x11, 0x74, 0x25, 0x95, 0x05, 0xa9, 0xc5,
+	0x49, 0x6c, 0xe0, 0x50, 0x30, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x2c, 0xfc, 0xc1, 0x3a, 0x59,
+	0x01, 0x00, 0x00,
+}
+
+func (m *OwnedDomain) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *OwnedDomain) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *OwnedDomain) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Parent) > 0 {
+		i -= len(m.Parent)
+		copy(dAtA[i:], m.Parent)
+		i = encodeVarintDomainOwnership(dAtA, i, uint64(len(m.Parent)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.Name) > 0 {
+		i -= len(m.Name)
+		copy(dAtA[i:], m.Name)
+		i = encodeVarintDomainOwnership(dAtA, i, uint64(len(m.Name)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
 }
 
 func (m *DomainOwnership) Marshal() (dAtA []byte, err error) {
@@ -120,9 +216,14 @@ func (m *DomainOwnership) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = l
 	if len(m.Domains) > 0 {
 		for iNdEx := len(m.Domains) - 1; iNdEx >= 0; iNdEx-- {
-			i -= len(m.Domains[iNdEx])
-			copy(dAtA[i:], m.Domains[iNdEx])
-			i = encodeVarintDomainOwnership(dAtA, i, uint64(len(m.Domains[iNdEx])))
+			{
+				size, err := m.Domains[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintDomainOwnership(dAtA, i, uint64(size))
+			}
 			i--
 			dAtA[i] = 0x12
 		}
@@ -148,6 +249,23 @@ func encodeVarintDomainOwnership(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return base
 }
+func (m *OwnedDomain) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Name)
+	if l > 0 {
+		n += 1 + l + sovDomainOwnership(uint64(l))
+	}
+	l = len(m.Parent)
+	if l > 0 {
+		n += 1 + l + sovDomainOwnership(uint64(l))
+	}
+	return n
+}
+
 func (m *DomainOwnership) Size() (n int) {
 	if m == nil {
 		return 0
@@ -159,8 +277,8 @@ func (m *DomainOwnership) Size() (n int) {
 		n += 1 + l + sovDomainOwnership(uint64(l))
 	}
 	if len(m.Domains) > 0 {
-		for _, s := range m.Domains {
-			l = len(s)
+		for _, e := range m.Domains {
+			l = e.Size()
 			n += 1 + l + sovDomainOwnership(uint64(l))
 		}
 	}
@@ -172,6 +290,120 @@ func sovDomainOwnership(x uint64) (n int) {
 }
 func sozDomainOwnership(x uint64) (n int) {
 	return sovDomainOwnership(uint64((x << 1) ^ uint64((int64(x) >> 63))))
+}
+func (m *OwnedDomain) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowDomainOwnership
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: OwnedDomain: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: OwnedDomain: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Name", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowDomainOwnership
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthDomainOwnership
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthDomainOwnership
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Name = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Parent", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowDomainOwnership
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthDomainOwnership
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthDomainOwnership
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Parent = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipDomainOwnership(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthDomainOwnership
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
 }
 func (m *DomainOwnership) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
@@ -238,7 +470,7 @@ func (m *DomainOwnership) Unmarshal(dAtA []byte) error {
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Domains", wireType)
 			}
-			var stringLen uint64
+			var msglen int
 			for shift := uint(0); ; shift += 7 {
 				if shift >= 64 {
 					return ErrIntOverflowDomainOwnership
@@ -248,23 +480,25 @@ func (m *DomainOwnership) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
+				msglen |= int(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
 			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
+			if msglen < 0 {
 				return ErrInvalidLengthDomainOwnership
 			}
-			postIndex := iNdEx + intStringLen
+			postIndex := iNdEx + msglen
 			if postIndex < 0 {
 				return ErrInvalidLengthDomainOwnership
 			}
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.Domains = append(m.Domains, string(dAtA[iNdEx:postIndex]))
+			m.Domains = append(m.Domains, &OwnedDomain{})
+			if err := m.Domains[len(m.Domains)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
