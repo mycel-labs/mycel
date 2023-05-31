08ef5adfa92568389d8ca667f6010c1ddaa56ca5
taryune <taryunelab@gmail.com>
2023-05-31 14:59:45 +0200

    create regstrationfee q

--- x/registry/client/cli/query.go
+++ x/registry/client/cli/query.go
@@ -29,6 +29,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdShowDomain())
 	cmd.AddCommand(CmdListDomainOwnership())
 	cmd.AddCommand(CmdShowDomainOwnership())
+	cmd.AddCommand(CmdDomainRegistrationFee())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
