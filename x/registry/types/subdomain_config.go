dc1f165b142f0eafb611abcc73f9b53d2be5a3f8
tarumi <taryunelab@gmail.com>
2023-12-13 12:23:32 +0900

    fix getRegistrationFee

--- x/registry/types/subdomain_config.go
+++ x/registry/types/subdomain_config.go
@@ -2,10 +2,8 @@ package types
 
 import (
 	"errors"
-	"fmt"
 
 	errorsmod "cosmossdk.io/errors"
-	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/mycel-domain/mycel/app/params"
@@ -21,51 +19,37 @@ func GetDefaultSubdomainConfig(baseFee int64) SubdomainConfig {
 	}
 }
 
-// TODO: This function will cause consensus failure
-func GetFeeByNameLength(base int64, baseFee int64, step int64) map[uint32]*Fee {
-	fees := make(map[uint32]*Fee, step)
-	baseDec, err := math.LegacyNewDecFromStr(fmt.Sprintf("%d", base))
-	if err != nil {
-		panic(err)
-	}
-	baseFeeDec, err := math.LegacyNewDecFromStr(fmt.Sprintf("%d", baseFee))
-	if err != nil {
-		panic(err)
-	}
-	for i := uint32(1); i <= uint32(step); i++ {
-		exponent := uint64(step+1) - uint64(i)
-		amount := baseFeeDec.Mul(baseDec.Power(exponent)).RoundInt()
-		fee := sdk.NewCoin(params.DefaultBondDenom, amount)
-		fees[i] = &Fee{
-			IsRegistrable: true,
-			Fee:           &fee,
-		}
-	}
-	return fees
-}
-
 func (config SubdomainConfig) GetRegistrationFee(name string, registrationPeriodInYear uint64) (amount sdk.Coin, err error) {
-	amount = *config.SubdomainRegistrationFees.DefaultFee
+    amount = *config.SubdomainRegistrationFees.DefaultFee
+    nameLength := uint32(len(name))
 
-	// Set amount if bylength found
-	if config.SubdomainRegistrationFees.FeeByName[name] != nil {
-		if config.SubdomainRegistrationFees.FeeByName[name].IsRegistrable {
-			amount = *config.SubdomainRegistrationFees.FeeByName[name].Fee
-		} else {
-			err = errorsmod.Wrap(errors.New(name), ErrSecondLevelDomainNotRegistrable.Error())
-		}
-	}
+    // Check if a fee by name exists
+    for _, feeByName := range config.SubdomainRegistrationFees.FeeByName {
+        if feeByName.Name == name {
+            if feeByName.IsRegistrable {
+                amount = *feeByName.Fee
+            } else {
+                err = errorsmod.Wrap(errors.New(name), ErrSecondLevelDomainNotRegistrable.Error())
+                return amount, err
+            }
+            break
+        }
+    }
 
-	// Set amount if byname found
-	if config.SubdomainRegistrationFees.FeeByLength[uint32(len(name))] != nil {
-		if config.SubdomainRegistrationFees.FeeByLength[uint32(len(name))].IsRegistrable {
-			amount = *config.SubdomainRegistrationFees.FeeByLength[uint32(len(name))].Fee
-		} else {
-			err = errorsmod.Wrap(errors.New(name), ErrSecondLevelDomainNotRegistrable.Error())
-		}
-	}
+    // Check if a fee by length exists if no specific name fee was found
+    if err == nil {
+        for _, feeByLength := range config.SubdomainRegistrationFees.FeeByLength {
+            if feeByLength.Length == nameLength {
+                if feeByLength.IsRegistrable {
+                    amount = *feeByLength.Fee
+                } else {
+                    err = errorsmod.Wrap(errors.New(name), ErrSecondLevelDomainNotRegistrable.Error())
+                }
+                break
+            }
+        }
+    }
 
-	amount.Amount = amount.Amount.Mul(sdk.NewInt(int64(registrationPeriodInYear)))
-
-	return amount, err
+    amount.Amount = amount.Amount.Mul(sdk.NewInt(int64(registrationPeriodInYear)))
+    return amount, err
 }
