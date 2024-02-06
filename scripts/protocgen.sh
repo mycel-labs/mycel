4280191e74419ce8bd57a0112f8d5097c792bd52
foxytanuki <biz@foxytanuki.dev>
2024-02-06 16:17:17 +0900

    Update scripts

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -4,17 +4,17 @@
 # docker build --pull --rm -f "contrib/devtools/Dockerfile" -t cosmossdk-proto:latest "contrib/devtools"
 # docker run --rm -v $(pwd):/workspace --workdir /workspace cosmossdk-proto sh ./scripts/protocgen.sh
 
-echo "Formatting protobuf files"
+echo "Formatting Protobuf files"
 find ./ -name "*.proto" -exec clang-format -i {} \;
 
 set -e
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./cosmos ./amino -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq | grep -v '^./cosmos/store/')
+proto_dirs=$(find ./mycel  -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq | grep -v '^./cosmos/store/')
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    # this regex checks if a proto file has its go_package set to cosmossdk.io/api/...
+    # this regex checks if a proto file has its go_package set to mycel/api/...
     # gogo proto files SHOULD ONLY be generated if this is false
     # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
     if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*cosmossdk.io/api' "$file" | grep -q ':0$'; then
@@ -25,16 +25,13 @@ done
 
 cd ..
 
-# generate tests proto code
-(cd testutil/testdata; buf generate)
-(cd baseapp/testutil; buf generate)
-(cd tests/integration/tx/internal; make codegen)
-
 # move proto files to the right places
-cp -r github.com/cosmos/cosmos-sdk/* ./
-cp -r cosmossdk.io/** ./
-rm -rf github.com cosmossdk.io
+echo "Move proto files to the right places"
+cp -r proto/github.com/mycel-domain/mycel/* ./
+rm -rf proto/github.com
 
+echo "Run go mod tidy to clean up go.mod and go.sum files"
 go mod tidy
 
+echo "Generating pulsar proto code"
 ./scripts/protocgen-pulsar.sh
